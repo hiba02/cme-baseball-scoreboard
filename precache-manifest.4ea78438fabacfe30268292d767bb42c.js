70668d06169a5d669d2e86af7bc0660039ec4e4e
ys <kangyoonsoon@gmail.com>
2021-10-19 22:08:41 -0400

    Updates

---
+++ precache-manifest.4ea78438fabacfe30268292d767bb42c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9f8b13c063355f73115b131e8340f3",
+    "url": "/cme-baseball-scoreboard/index.html"
+  },
+  {
+    "revision": "1e68d6972dc816227c9b",
+    "url": "/cme-baseball-scoreboard/static/css/main.f1b148a5.chunk.css"
+  },
+  {
+    "revision": "2a42559633f164062cb5",
+    "url": "/cme-baseball-scoreboard/static/js/2.99b1f246.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cme-baseball-scoreboard/static/js/2.99b1f246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e68d6972dc816227c9b",
+    "url": "/cme-baseball-scoreboard/static/js/main.c5062859.chunk.js"
+  },
+  {
+    "revision": "abc47bd99fd5c77d22dc",
+    "url": "/cme-baseball-scoreboard/static/js/runtime-main.fa528049.js"
+  },
+  {
+    "revision": "29605a9c3c2acb0510a8755154b2d09b",
+    "url": "/cme-baseball-scoreboard/static/media/baseball_park3.29605a9c.jpg"
+  },
+  {
+    "revision": "d57c4039d4bfcdff5ead57ef8dff2459",
+    "url": "/cme-baseball-scoreboard/static/media/glove-clipart-6.d57c4039.png"
+  },
+  {
+    "revision": "73e7a65df99e77d7a313557a5c4e2030",
+    "url": "/cme-baseball-scoreboard/static/media/header_baseball4.73e7a65d.jpg"
+  },
+  {
+    "revision": "2fe04e35a9526c671f3d085c9a224ef4",
+    "url": "/cme-baseball-scoreboard/static/media/hitter-logo.2fe04e35.png"
+  },
+  {
+    "revision": "166b83e87fb87c734a8581474fc5ff9f",
+    "url": "/cme-baseball-scoreboard/static/media/pitcher-logo.166b83e8.png"
+  }
+]);
